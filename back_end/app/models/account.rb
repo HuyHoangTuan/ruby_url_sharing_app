594cd640a96fd8533805264d93bc237b55bdb643
HuyHt <kubin2712@gmail.com>
2024-09-26 00:57:35 +0700

    update

---
+++ back_end/app/models/account.rb
@@ -0,0 +1,6 @@
+class Account < ApplicationRecord
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 8 }
+end
